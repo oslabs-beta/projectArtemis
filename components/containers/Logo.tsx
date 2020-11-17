e65a348265266da5163853e197ac676a4d48349a
Erick M <erickmlndz@protonmail.com>
2020-11-17 11:16:25 -0800

    minor formatting fix

--- components/containers/Logo.tsx
+++ components/containers/Logo.tsx
@@ -1,28 +1,7 @@
 import React from "https://esm.sh/react"
 
-export  function Hamburger() {
-    return (
-        <div>
-     <img src="/hamburger.svg"></img>
+export const Hamburger = () => <img src="/hamburger.svg"></img>
 
-     </div>
-    )
-}
+export const Git = () => <img src="/github.svg"></img>
 
-export function Git() {
-    return (
-        <div>
-     <img src="/github.svg"></img>
-
-     </div>
-    )
-}
-
-export function Twit() {
-    return (
-        <div>
-     <img src="/twitter.svg"></img>
-
-     </div>
-    )
-}
+export const Twit = () => <img src="/twitter.svg"></img>

a8b54f23757cf74a56dd049e9cf49790963c5a6f
Erick M <erickmlndz@protonmail.com>
2020-11-17 11:39:03 -0800

    merge - success ( design layout WIP )

--- components/MenuBar.tsx
+++ components/MenuBar.tsx
@@ -1,30 +1,26 @@
 import React from "https://esm.sh/react";
-import { MenuItems }  from "./containers/MenuItems.tsx"
-import { Hamburger, Git, Twit } from "./containers/Logo.tsx"
+import { MenuItems }  from "./menubar-container/MenuItems.tsx"
+import { Hamburger, Git, Twit } from "./Logos.tsx"
 const MenuBar = () => {
   return (
     <nav className="NavbarItem">
-         <h1 className="navbar-logo"><Hamburger /></h1>
-         <div className="menu-icon">
-
-        </div>
-         <ul>
-           {/* maps over the array that holds our links for the menubar */}
-           {MenuItems.map((item, index)=> {
-             return (
-
-           <li key={index}>
-           <a className={item.cName} href={item.url}>
-             {item.title}
-           </a>
-           </li>
-             )
-            })}
-         </ul>
-         <Git />
-         <Twit />
-       </nav>
-
+      <h1 className="navbar-logo"><Hamburger /></h1>
+      <div className="menu-icon">
+      </div>
+      <ul>
+        {/* maps over the array that holds our links for the menubar */}
+        {MenuItems.map((item, index)=> {
+          return (
+          <li key={index}>
+          <a className={item.cName} href={item.url}>
+          {item.title}
+          </a>
+          </li>
+        )})}
+      </ul>
+      <Git />
+      <Twit />
+    </nav>
   );
 }
 

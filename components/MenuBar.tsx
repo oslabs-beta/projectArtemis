0e892194065e83dfd8abee19978fad4ad629bdca
Erick M <erickmlndz@protonmail.com>
2020-11-17 15:52:03 -0800

    menubar layout complete

--- components/MenuBar.tsx
+++ components/MenuBar.tsx
@@ -3,21 +3,19 @@ import { MenuItems }  from "./menubar-container/MenuItems.tsx"
 import { Hamburger, Git, Twit } from "./Logos.tsx"
 const MenuBar = () => {
   return (
-    <div className="menu-items">
+    <div className="container-menubar">
       <Hamburger />
-      <ul className='menubar-items_list-links'>
+      <div className='menubar-items_list-links'>
         {/* maps over the array that holds our links for the menubar */}
-        <div className="menubar-sublist">
         {MenuItems.map((item, index)=> {
           return (
-          <li key={index} style={{"marginTop": "10px"}}>
+          <div key={index} style={{"marginTop": "10px"}}>
           <a className={item.cName} href={item.url}>
           {item.title}
           </a>
-          </li>
+          </div>
         )})}
       </div>
-      </ul>
       <div className='menubar-items_social-icons'>
         <Git />
         <Twit />

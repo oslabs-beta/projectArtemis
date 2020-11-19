6ae0dee16aff2de4d5b6735212906651fbe5bedb
Scott Burman <scottburs@gmail.com>
2020-11-19 12:11:43 -0800

    HamburgerDrop

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
-import { Import } from 'https://deno.land/x/aleph/mod.ts'
-import React from 'https://esm.sh/react'
-import MenuBar from '../components/MenuBar.tsx'
-import AnalyticsContainer from '../components/AnalyticsContainer.tsx'
+import { Import } from 'https://deno.land/x/aleph/mod.ts';
+import React, { useState } from 'https://esm.sh/react';
+import MenuBar from '../components/MenuBar.tsx';
+import AnalyticsContainer from '../components/AnalyticsContainer.tsx';
+import HamburgerDrop from '../components/HamburgerDrop.tsx';
 
 export default function Home() {
-    return (
-        <div className='container-gui'>
-            <Import from="../style/index.css" />
-            <MenuBar />
-            <AnalyticsContainer />
-        </div>
-    )
+	const [ state, setState ] = useState(true);
+	return (
+		<div className="container-gui">
+			<Import from="../style/index.css" />
+			{state ? <MenuBar state={state} setState={setState} /> : <HamburgerDrop state={state} setState={setState} />}
+			<AnalyticsContainer />
+		</div>
+	);
 }

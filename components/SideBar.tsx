0db06bf125ee006c077ed0dc44277e7ef23e0aa2
Erick M <erickmlndz@protonmail.com>
2020-11-19 17:14:57 -0800

    commented application

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -4,6 +4,7 @@ import ListOfQueries from './menubar-container/ListOfQueries.tsx';
 
 const SideBar = () => {
   // be able to swap between two different menus
+  // pass down to state as props so components can toggle state
   const [toggle, setToggle] = useState<boolean>(true);
 
   return toggle ? (
--- components/menubar-container/MenuBar.tsx
+++ components/menubar-container/MenuBar.tsx
@@ -9,6 +9,14 @@ interface Props {
 
 const MenuBar = (props: Props) => {
   const { toggle, setToggle } = props;
+  // get link info from data folder within menubar-container
+  const menuLinks = MenuItems.map((linkInfo, index) => (
+    <div className="wrapper_navLinks">
+      <a className="sidebar_nav-links" href={linkInfo.url} key={index}>
+        {linkInfo.title}
+      </a>
+    </div>
+  ));
 
   return (
     <div className="flex-menubar container-sidebar">
@@ -16,16 +24,7 @@ const MenuBar = (props: Props) => {
         <Hamburger />
       </div>
       <div className="menubar-items_list-links">
-        {/* maps over the array that holds our links for the menubar */}
-        {MenuItems.map((item, index) => {
-          return (
-            <div className='wrapper_navLinks'>
-              <a className='sidebar_nav-links' href={item.url}>
-                {item.title}
-              </a>
-            </div>
-          );
-        })}
+        {menuLinks}
       </div>
       <div className="menubar-items_social-icons">
         <div className="social-icon">

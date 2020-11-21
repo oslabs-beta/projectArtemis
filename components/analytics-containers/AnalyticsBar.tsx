ea379d99c4fcda9336c82749df1f9d3ebf27166d
Erick M <erickmlndz@protonmail.com>
2020-11-20 17:00:30 -0800

    integrated graph component to gui

--- components/analytics-containers/AnalyticsBar.tsx
+++ components/analytics-containers/AnalyticsBar.tsx
@@ -1,7 +1,29 @@
-import React from 'https://esm.sh/react';
+import React, { useState, useEffect } from 'https://esm.sh/react';
+import QuerySpeed from '../graphs/QuerySpeed.tsx';
+
+const createData = (() => {
+  let count = 0;
+  return () => {
+    count += 1;
+    return {
+      [`query${count}`]: Math.random(),
+    };
+  };
+})();
 
 const AnalyticsBar = () => {
-  return <div className="container-analytics"></div>;
+  const [queryData, setQueryData] = useState([]);
+
+  useEffect(() => {
+    setInterval(() => {
+      setQueryData((prevQueryData) => [...prevQueryData, createData()]);
+    }, 5000);
+  }, []);
+  return (
+    <div className="container-analytics">
+      <QuerySpeed queryData={queryData} />
+    </div>
+  );
 };
 
 export default AnalyticsBar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,16 @@
 import { Import } from 'https://deno.land/x/aleph/mod.ts';
 import React, { useState, useEffect } from 'https://esm.sh/react';
 import AnalyticsContainer from '../components/AnalyticsContainer.tsx';
-import QuerySpeed from '../components/graph/QuerySpeed.tsx';
 import SideBar from '../components/SideBar.tsx';
 
 // let's try not to pollute our index.tsx - components that encompass a lot of small components
 
-const createData = (() => {
-  let count = 0;
-  return () => {
-    count += 1;
-    return {
-      [`query${count}`]: Math.random(),
-    };
-  };
-})();
-
 export default function Home() {
-  const [queryData, setQueryData] = useState([]);
-
-  useEffect(() => {
-    setInterval(() => {
-      setQueryData((prevQueryData) => [...prevQueryData, createData()]);
-    }, 5000);
-  }, []);
-
   return (
     <div className="container-gui">
-      {/* <Import from="../style/index.css" /> */}
-      {/* <SideBar /> */}
-      {/* <AnalyticsContainer /> */}
-      <QuerySpeed queryData={queryData}/>
+      <Import from="../style/index.css" />
+      <SideBar />
+      <AnalyticsContainer />
     </div>
   );
 }

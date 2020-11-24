57c7955752fc7d080f10f763a14d8487e5f4ca03
greg-dixon <gdixon529@gmail.com>
2020-11-24 10:50:14 -0800

    refactored metrics function by condensing them into one

--- functions/aggregateMetrics.ts
+++ functions/aggregateMetrics.ts
@@ -1,29 +1,44 @@
-const queriesPerApi = (arr) => {
+const aggregateMetrics = (arr) => {
     const apis = {}
+    let latencySum = 0;
+    let latencyMax = 0;
+    let sizeSum = 0;
+    let sizeMax = 0;
+    const successfulArr = []
+    const errorArr = []
     arr.forEach(el => {
+        //Queries per API
         if (!apis.hasOwnProperty(el.api)) {
             apis[el.api] = 1;
         }
         else {
             apis[el.api]++
         }
-    })
-    return apis
-}
-
-const latencyMetrics = (arr) => {
-    let sum = 0;
-    let count = 0;
-    let max = 0;
-    arr.forEach(el => {
-        count++
-        sum += el.latency
-        if (el.latency > max) {
-            max = el.latency
+        //Latency
+        latencySum += el.latency
+        if (el.latency > latencyMax) {
+            latencyMax = el.latency
         }
+        //Size
+        sizeSum += el.dataSize
+        if (el.dataSize > sizeMax) {
+            sizeMax = el.dataSize
+        }
+        //Query vs. Error Frequency
+        if (el.successfulQuery === true) {
+            successfulArr.push(el.successfulQuery)}
+        else {
+            errorArr.push(el.successfulQuery)
+      }
     });
+
     return {
-        average: sum / count,
-        max
+        apis,
+        latencyAvg : latencySum / arr.length,
+        latencyMax,
+        sizeAvg: sizeSum / arr.length,
+        sizeMax,
+        queryFrequency: successfulArr.length,
+        errorFrequency: errorArr.length,
     }
 }

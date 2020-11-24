265dec2df227863db2d5f0da8a2af090e6f6428a
Erick M <erickmlndz@protonmail.com>
2020-11-23 20:04:15 -0800

    removed test custom hook

--- components/AnalyticsContainer.tsx
+++ components/AnalyticsContainer.tsx
@@ -1,13 +1,10 @@
-import React, { useState, useEffect } from 'https://esm.sh/react';
+import React from 'https://esm.sh/react';
 import TabBar from './analytics-containers/TabBar.tsx';
 import AnalyticsBar from './analytics-containers/AnalyticsBar.tsx';
 import useViewController from './utils/useViewController.ts';
 import '../style/main-view.css';
 
-import useTest from './utils/useTest.ts';
-
 const AnalyticsContainer = () => {
-  const [queryData] = useTest();
   const [viewIndex, updateViewIndex] = useViewController();
 
   return (

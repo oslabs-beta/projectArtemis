356674b7b5774a6c4e6bbe5be33da5e19e35bd1f
Erick M <erickmlndz@protonmail.com>
2020-12-02 11:24:51 -0800

    stabalized artemis - set initial values to null

--- components/analyticsComponents/graphs/QueryPerAPIGraph.tsx
+++ components/analyticsComponents/graphs/QueryPerAPIGraph.tsx
@@ -2,61 +2,93 @@ import React from 'https://esm.sh/react';
 import { Bar } from 'https://cdn.skypack.dev/react-chartjs-2';
 import '../../../style/graphs.css';
 
+interface Result {
+  apis: {};
+  latencyAvg: string;
+  latencyMax: string;
+  sizeAvg: string;
+  sizeMax: string;
+  queryTotal: any;
+  queryFrequency: number;
+  errorFrequency: number;
+}
+
 interface Props {
-	aggregateMetrics: object | {};
+  aggregateMetrics: Result | null;
 }
 
 const QueryPerAPIGraph = (props: Props) => {
-const { aggregateMetrics } = props;
-	const amounts: any = [];
-	const labels: any = [];
-	let topAmount: number = 0;
-	let topAPI: string = "";
-	for (let x in aggregateMetrics.apis) {
-		labels.push(x)
-		amounts.push(aggregateMetrics.apis[x]);
-		if (aggregateMetrics.apis[x] > topAmount) {
-			topAmount = aggregateMetrics.apis[x];
-			topAPI = x;
-		}
-	}
-	const data = {
-		labels,
-		datasets: [
-			{
-				backgroundColor: ['#36A2EB', '#FF6384', '#b6fc03', '#b503fc', '#fc037b', '#fcfc03'],
-				hoverBackgroundColor: ['#36A2EB', '#FF6384', '#b6fc03', '#b503fc', '#fc037b', '#fcfc03'],
-				data: amounts
-			}
-		]
-	};
-	const options = {
-		responsive: true,
-		maintainAspectRatio: false,
-		title: {
-			display: true,
-			text: `Most Frequently Queried API: ${topAPI}`
-		},
-		onClick: function(e, item) {
-			console.log(item);
-		}
-	};
+  const { aggregateMetrics } = props;
+  const calAmount = (string: string) => {
+    const amounts: any = [];
+    const labels: any = [];
+    let topAmount: number = 0;
+    let topAPI: string = '';
+    for (let x in aggregateMetrics.apis) {
+      labels.push(x);
+      amounts.push(aggregateMetrics.apis[x]);
+      if (aggregateMetrics.apis[x] > topAmount) {
+        topAmount = aggregateMetrics.apis[x];
+        topAPI = x;
+      }
+    }
+    if (string === 'amounts') return amounts;
+    else if (string === 'labels') return labels;
+    else if (string === 'topAPI') return topAPI;
+  };
+  const data = {
+    labels: aggregateMetrics ? calAmount('labels') : [],
+    datasets: [
+      {
+        backgroundColor: [
+          '#36A2EB',
+          '#FF6384',
+          '#b6fc03',
+          '#b503fc',
+          '#fc037b',
+          '#fcfc03',
+        ],
+        hoverBackgroundColor: [
+          '#36A2EB',
+          '#FF6384',
+          '#b6fc03',
+          '#b503fc',
+          '#fc037b',
+          '#fcfc03',
+        ],
+        data: aggregateMetrics ? calAmount('amounts') : [],
+      },
+    ],
+  };
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    title: {
+      display: true,
+      text: `Most Frequently Queried API: ${
+        aggregateMetrics ? calAmount('topAPI') : ''
+      }`,
+    },
+    onClick: function (e, item) {
+      console.log(item);
+    },
+  };
 
-	const legend = {
-		display: false
-	};
+  const legend = {
+    display: false,
+  };
 
-	return (
-		<div className="graph">
-			<Bar
-				data={data}
-				options={options}
-				legend={legend}
-				aria-label="display-query-success-and-failures"
-				role="img"
-			/>
-		</div>
-	);
+  return (
+    <div className="graph">
+      <Bar
+        data={data}
+        options={options}
+        legend={legend}
+        aria-label="display-query-success-and-failures"
+        role="img"
+      />
+    </div>
+  );
 };
 
 export default QueryPerAPIGraph;
